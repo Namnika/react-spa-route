75a60d135018fd9aeadce8d6ad984a548982c745
Namnika <janbandhunamnika@gmail.com>
2025-07-01 11:09:22 +0530

    Create search page to test query params

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import UserProfile from './pages/UserProfile';
+import SearchPage from './pages/SearchPage';
 import './App.css';
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/about' element={<About />}/>
         {/* sending id to the UserProfile component */}
         <Route path='/user/:id' element ={<UserProfile />}/> 
+        <Route path='/search' element={<SearchPage />}/>
       </Routes>
     </Router>
   );
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,16 @@
 import React from "react";
-import { useParams, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 function SearchPage (){
     const [searchParams] = useSearchParams()
-    const query = searchParams.get
+    const query = searchParams.get("query")
+    const page = searchParams.get("page")
+
+    return(
+        <div>
+            Query: {query} on Page Number page: {page}
+        </div>
+    )
 }
+
+export default SearchPage
